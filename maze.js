af62c0fb949295dd3dbfb70d944075cd7c4f65bb
Gabriel Ricardo <gabrielricardo7@gmail.com>
2021-09-30 16:25:30 -0300

    Faça cada célula do labirinto uma div

---
+++ maze.js
@@ -0,0 +1,27 @@
+// W = wall, S = start, F = finish
+const map = [
+    "WWWWWWWWWWWWWWWWWWWWW",
+    "W   W     W     W W W",
+    "W W W WWW WWWWW W W W",
+    "W W W   W     W W   W",
+    "W WWWWWWW W WWW W W W",
+    "W         W     W W W",
+    "W WWW WWWWW WWWWW W W",
+    "W W   W   W W     W W",
+    "W WWWWW W W W WWW W F",
+    "S     W W W W W W WWW",
+    "WWWWW W W W W W W W W",
+    "W     W W W   W W W W",
+    "W WWWWWWW WWWWW W W W",
+    "W       W       W   W",
+    "WWWWWWWWWWWWWWWWWWWWW",
+];
+
+let variable = 0;
+for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+        document.querySelector("body").appendChild(document.createElement("div"));
+        variable++;
+        console.log(variable);
+    }
+}
