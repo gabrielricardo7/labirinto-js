f59338f8b71c0ba43bce7f333ac6beabe0fc6b4d
Gabriel Ricardo <gabrielricardo7@gmail.com>
2021-10-01 00:20:36 -0300

    Registrar posição atual do jogador no labirinto e movimento

--- maze.js
+++ maze.js
@@ -20,6 +20,9 @@ const map = [
 const player = document.createElement("div");
 player.id = "player";
 
+let playerX = 0;
+let playerY = 0;
+
 for (let i = 0; i < map.length; i++) {
     const line = document.createElement("div");
     line.id = `${"line" + i}`;
@@ -33,13 +36,41 @@ for (let i = 0; i < map.length; i++) {
         } else if (map[i][j] === "S") {
             cell.className = "start";
         } else if (map[i][j] === "F") {
-            cell.className = "finish";
+            cell.className = "cell finish";
         } else {
-            cell.className = "space";
+            cell.className = "cell";
         }
-        cell.classList.add("cell");
+        cell.classList.add("block");
         line.appendChild(cell);
     }
 }
 
 document.querySelector(".start").appendChild(player);
+
+playerY = player.parentElement.parentElement.id.substring(4);
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+
+    if (keyName === "ArrowUp") {
+        if (player.parentElement.parentElement.previousElementSibling.children[playerX].classList[0] === "cell") {
+            player.parentElement.parentElement.previousElementSibling.children[playerX].appendChild(player);
+            playerY--;
+        }
+    } else if (keyName === "ArrowDown") {
+        if (player.parentElement.parentElement.nextElementSibling.children[playerX].classList[0] === "cell") {
+            player.parentElement.parentElement.nextElementSibling.children[playerX].appendChild(player);
+            playerY++;
+        }
+    } else if (keyName === "ArrowLeft") {
+        if (player.parentElement.previousElementSibling.classList[0] === "cell") {
+            player.parentElement.previousElementSibling.appendChild(player);
+            playerX--;
+        }
+    } else if (keyName === "ArrowRight") {
+        if (player.parentElement.nextElementSibling.classList[0] === "cell") {
+            player.parentElement.nextElementSibling.appendChild(player);
+            playerX++;
+        }
+    }
+});
